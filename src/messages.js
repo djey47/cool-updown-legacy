ebd334d83ec434358a77984e573822f171e330f2
djey <j.canler@gmail.com>
2018-05-24 01:05:25 +0200

    feat: enhance ping with effective config + WIP ON/OFF services

--- src/messages.js
+++ src/messages.js
@@ -6,6 +6,10 @@ const messages = {
   exitNotice: '- CTRL+C to stop server -',
   onCron: 'Up\'ed with CRON Job!',
   offCron: 'Down\'ed with CRON Job!',
+  wakeOK: 'WOL packets sent!',
+  wakeKO: 'Could not send WOL packets!',
+  sshOK: 'Connected to host via SSH!',
+  sshKO: 'Could not connect to host via SSH!',
 };
 
 module.exports = messages;
