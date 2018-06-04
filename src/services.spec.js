b8b4d1d653cf9eefa6c3bd7f23d50d50e01d7ae9
Jérôme Canler <jerome.canler@soat.fr>
2018-06-04 14:02:37 +0200

    refactor: simplify async usage and add on/off unit tests

--- src/services.spec.js
+++ src/services.spec.js
@@ -1,11 +1,21 @@
-const { ping } = require('./services');
+const { ping, on, off } = require('./services');
 
 const mockSend = jest.fn();
+const mockStatus = jest.fn();
 const mockGatewayPing = jest.fn();
 const mockSSHConnect = jest.fn();
+const mockSSHExecCommand = jest.fn();
+const mockSSHDispose = jest.fn();
+const mockWOLWake = jest.fn();
+
+jest.mock('wake_on_lan', () => ({
+  wake: (a, o, f) => mockWOLWake(a, o, f),
+}));
 
 jest.mock('node-ssh', () => jest.fn(() => ({
   connect: o => mockSSHConnect(o),
+  execCommand: c => mockSSHExecCommand(c),
+  dispose: () => mockSSHDispose(),
 })));
 
 jest.mock('./helpers/systemGateway', () => ({
@@ -13,19 +23,33 @@ jest.mock('./helpers/systemGateway', () => ({
 }));
 
 const res = {
+  status: c => mockStatus(c),
   send: msg => mockSend(msg),
 };
 
 describe('services functions', () => {
   beforeEach(() => {
     mockSend.mockReset();
+    mockStatus.mockReset();
     mockGatewayPing.mockReset();
     mockSSHConnect.mockReset();
+    mockSSHExecCommand.mockReset();
+    mockSSHDispose.mockReset();
+    mockWOLWake.mockReset();
 
     // Ping OK
     mockGatewayPing.mockImplementation((h, f) => {
       f();
     });
+
+    // WOL OK
+    mockWOLWake.mockImplementation((a, o, f) => {
+      f();
+    });
+
+    mockStatus.mockImplementation(() => res);
+
+    mockSSHExecCommand.mockImplementation(() => Promise.resolve({ stdout: '', stderr: '' }));
   });
 
   const appState = {
@@ -49,6 +73,7 @@ describe('services functions', () => {
           privateKey: 'Key Path',
           username: 'User',
         });
+        expect(mockSSHDispose).toHaveBeenCalled();
         expect(mockSend).toHaveBeenCalled();
         expect(mockSend.mock.calls[0][0]).toMatchSnapshot();
         done();
@@ -69,6 +94,7 @@ describe('services functions', () => {
       ping({}, res, state).then(() => {
         expect(mockGatewayPing).toHaveBeenCalled();
         expect(mockSSHConnect).toHaveBeenCalled();
+        expect(mockSSHDispose).toHaveBeenCalled();        
         expect(mockSend).toHaveBeenCalled();
         expect(mockSend.mock.calls[0][0]).toMatchSnapshot();
         done();
@@ -87,6 +113,7 @@ describe('services functions', () => {
       ping({}, res, state).then(() => {
         expect(mockGatewayPing).toHaveBeenCalled();
         expect(mockSSHConnect).toHaveBeenCalled();
+        expect(mockSSHDispose).toHaveBeenCalled();        
         expect(mockSend).toHaveBeenCalled();
         expect(mockSend.mock.calls[0][0]).toMatchSnapshot();
         done();
@@ -124,6 +151,85 @@ describe('services functions', () => {
       ping({}, res, state).then(() => {
         expect(mockSend).toHaveBeenCalled();
         expect(mockSend.mock.calls[0][0]).toMatchSnapshot();
+      });
+    });
+  });
+
+  describe('on function', () => {
+    it('should invoke wol and generate correct response on success', () => {
+      // given-when
+      on(undefined, res);
+
+      // then
+      expect(mockWOLWake).toHaveBeenCalled();
+      expect(mockWOLWake.mock.calls[0][0]).toEqual('FF:FF:FF:FF:FF:FF:FF:FF');
+      expect(mockWOLWake.mock.calls[0][1]).toEqual({ address: '255.255.255.255' });
+      expect(mockSend.mock.calls[0][0]).toMatchSnapshot();
+    });
+
+    it('should invoke wol and generate correct response on failure', () => {
+      // given
+      // WOL KO
+      mockWOLWake.mockImplementation((a, o, f) => {
+        f({});
+      });
+
+      // when
+      on(undefined, res);
+      
+      // then
+      expect(mockWOLWake).toHaveBeenCalled();
+      expect(mockStatus).toHaveBeenCalledWith(500);
+      expect(mockSend.mock.calls[0][0]).toMatchSnapshot();
+    });
+  });
+
+  describe('off function', () => {
+    it('should invoke SSH and generate correct response on success', (done) => {
+      // given-when-then
+      off(undefined, res).then(() => {
+        expect(mockSSHConnect).toHaveBeenCalledWith({
+          host: 'Host Name',
+          port: 22,
+          privateKey: 'Key Path',
+          username: 'User',
+        });
+        expect(mockSSHExecCommand).toHaveBeenCalledWith('OFF Command');
+        expect(mockSSHDispose).toHaveBeenCalled();
+        expect(mockSend.mock.calls[0][0]).toMatchSnapshot();
+        done();
+      });
+    });
+
+    it('should invoke SSH and generate correct response on command failure', (done) => {
+      // given
+      // ExecCommand KO
+      mockSSHExecCommand.mockImplementation(() => Promise.reject());
+      
+      // when-then
+      off(undefined, res).then(() => {
+        expect(mockSSHConnect).toHaveBeenCalled();
+        expect(mockSSHExecCommand).toHaveBeenCalled();
+        expect(mockSSHDispose).toHaveBeenCalled();
+        expect(mockStatus).toHaveBeenCalledWith(500);
+        expect(mockSend.mock.calls[0][0]).toMatchSnapshot();
+        done();
+      });
+    });
+
+    it('should invoke SSH and generate correct response on connect failure', (done) => {
+      // given
+      // Connect KO
+      mockSSHConnect.mockImplementation(() => Promise.reject());
+      
+      // when-then
+      off(undefined, res).then(() => {
+        expect(mockSSHConnect).toHaveBeenCalled();
+        expect(mockSSHExecCommand).not.toHaveBeenCalled();
+        expect(mockSSHDispose).toHaveBeenCalled();
+        expect(mockStatus).toHaveBeenCalledWith(500);
+        expect(mockSend.mock.calls[0][0]).toMatchSnapshot();
+        done();
       });
     });
   });
