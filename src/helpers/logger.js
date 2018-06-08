38653147d853a4904ee72d693121f40fbd3c41e2
Jérôme Canler <jerome.canler@soat.fr>
2018-06-08 16:07:36 +0200

    chore: handle logging to file via winston + add logs:all command + update README

--- src/helpers/logger.js
+++ src/helpers/logger.js
@@ -1,13 +1,27 @@
 const winston = require('winston');
+const shell = require('shelljs');
+const appRootDir = require('app-root-dir');
+const path = require('path');
 
-/* DEFAULT LOGGER */
+/* DEFAULT LOGGER: appends to console and app.log file */
 
-module.exports = new winston.Logger({
+const logsDirectory = path.join(appRootDir.get(), 'logs');
+shell.mkdir('-p', logsDirectory);
+
+const logsFile = path.join(logsDirectory, 'app.log');
+
+const Logger = new winston.Logger({
   level: 'info',
   handleExceptions: false,
   transports: [
     new winston.transports.Console({
       timestamp: true,
     }),
+    new winston.transports.File({
+      filename: logsFile,
+      json: false,
+    }),
   ],
 });
+
+module.exports = Logger;
