3bc9c55ad927a348a91e69ee67114881709b61e1
Jérôme Canler <jerome.canler@soat.fr>
2018-06-13 16:30:58 +0200

    feat: (ping) display application uptime in days, hours, minutes

---
+++ src/helpers/date.js
@@ -0,0 +1,35 @@
+const MS_IN_1_S = 1000;
+const MS_IN_1_M = MS_IN_1_S * 60;
+const MS_IN_1_H = MS_IN_1_M * 60;
+const MS_IN_1_D = MS_IN_1_H * 24;
+
+/**
+ * @returns Object with fields days, hours, minutes, seconds, milliseconds
+ */
+function getTimeDetails(durationMs) {
+  let milliseconds = durationMs;
+
+  const days = Math.floor(milliseconds / MS_IN_1_D);
+  milliseconds %= MS_IN_1_D;
+
+  const hours = Math.floor(milliseconds / MS_IN_1_H);
+  milliseconds %= MS_IN_1_H;
+
+  const minutes = Math.floor(milliseconds / MS_IN_1_M);
+  milliseconds %= MS_IN_1_M;
+
+  const seconds = Math.floor(milliseconds / MS_IN_1_S);
+  milliseconds %= MS_IN_1_S;
+
+  return {
+    milliseconds,
+    seconds,
+    minutes,
+    hours,
+    days,
+  };
+}
+
+module.exports = {
+  getTimeDetails,
+};
--- src/helpers/format.js
+++ src/helpers/format.js
@@ -7,9 +7,35 @@ const REGEX_TEMPLATE = /{([\s\S]+?)}/g;
  * @return string with replaced {item} by corresponding value in templateContext
  */
 function interpolate(template, templateContext) {
-  return template.replace(REGEX_TEMPLATE, (match, submatch) => templateContext[submatch] || `{?${submatch}?}`);
+  return template.replace(
+    REGEX_TEMPLATE,
+    (match, submatch) => ({}.hasOwnProperty.call(templateContext, submatch) ?
+      templateContext[submatch] : `{?${submatch}?}`),
+  );
+}
+
+/**
+ * @private
+ */
+function toDurationUnit(timeDetail, unitLabel) {
+  return timeDetail ? `${timeDetail} ${unitLabel}` : '';
+}
+
+/**
+ * @return string with readable time (till minutes)
+ */
+function toHumanDuration(timeDetails) {
+  if (!timeDetails) return '';
+
+  const { days, hours, minutes } = timeDetails;
+  const duration = `${toDurationUnit(days, 'day(s)')} ${toDurationUnit(hours, 'hour(s)')} ${toDurationUnit(minutes, 'minute(s)')}`
+    .trim();
+
+  return duration.length ?
+    duration : 'less than one minute';
 }
 
 module.exports = {
   interpolate,
+  toHumanDuration,
 };
